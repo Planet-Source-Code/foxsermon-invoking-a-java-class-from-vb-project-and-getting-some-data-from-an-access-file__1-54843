73f3e92de3f9cd1e068124102b0aaeb6b329e7d8
pscbot <pscbot@saas.bg>
2020-09-23 05:13:25 +0300

    Initial commit

---
+++ MyTest.java
@@ -0,0 +1,7 @@
+//************************************************** 
+public class MyTest { 
+   public int myfunction(int value1, int value2) { 
+     return value1+value2; 
+   } 
+} 
+//*************************************************** 
